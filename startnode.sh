5dccc4250a5627af7b4ada943c3d0da4402ff6dd
silentnoname <635397523pl@gamil.com>
2023-02-05 01:29:11 +0800

    fix

--- startnode.sh
+++ startnode.sh
@@ -7,6 +7,7 @@ gra=$1
 sudo rm -rf $HOME/.ironfish
 sudo docker stop ironfish
 sudo docker rm ironfish
+sudo docker pull ghcr.io/iron-fish/ironfish:latest
 sudo docker run --restart=always --name ironfish -d --tty --interactive --network host  --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start
 sleep 10
 sudo docker exec -it  ironfish ironfish config:set blockGraffiti $gra
