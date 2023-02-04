d90d00a24810a435b1b09699798b4dbeda1b60a4
silentnoname <635397523pl@gamil.com>
2023-02-05 02:34:46 +0800

    only use 2core

--- startnode.sh
+++ startnode.sh
@@ -8,7 +8,7 @@ sudo rm -rf $HOME/.ironfish
 sudo docker stop ironfish
 sudo docker rm ironfish
 sudo docker pull ghcr.io/iron-fish/ironfish:latest
-sudo docker run --restart=always --name ironfish -d --tty --interactive --network host  --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start
+sudo docker run --restart=always --name ironfish -d --tty --cpus=2 --interactive --network host  --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start
 sleep 10
 sudo docker exec -it  ironfish ironfish config:set blockGraffiti $gra
 sudo docker exec -it ironfish ironfish config:set enableTelemetry true
