d95fd84da7f0155ac0c45991e71bc6aee107313b
silentnoname <635397523pl@gamil.com>
2023-02-05 01:12:59 +0800

    add initial version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ironfish-testnet-3
+
+## start node 
+```
+git clone https://github.com/silentnoname/ironfish-testnet-3.git
+cd ironfish-testnet-3
+sudo chmod +x startnode.sh 
+./startnode.sh  <graffiti>
+```
+
+## check node status
+```
+sudo docker exec -it  ironfish ironfish status
+```
---
+++ startnode.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+    echo "Please input your graffiti"
+    exit 1
+fi
+gra=$1
+sudo rm -rf $HOME/.ironfish
+sudo docker stop ironfish
+sudo docker rm ironfish
+sudo docker run --restart=always --name ironfish -d --tty --interactive --network host  --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start
+sleep 10
+sudo docker exec -it  ironfish ironfish config:set blockGraffiti $gra
+sudo docker exec -it ironfish ironfish config:set enableTelemetry true
